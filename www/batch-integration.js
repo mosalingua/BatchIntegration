ca6238c1bb148c253f4d24eb09e0d2adf6f7a482
Jeroen De Swaef <j@recallq.com>
2015-08-11 16:43:46 +0200

    fix: name to call Plugin

--- www/batch-integration.js
+++ www/batch-integration.js
@@ -4,7 +4,7 @@ var batchIntegration = {
         cordova.exec(
             successCallback, // success callback function
             errorCallback, // error callback function
-            'com.mosalingua.plugin.BatchIntegration', // mapped to our native Java class
+            'BatchIntegration', // mapped to our native Java class
             'changeLanguage', // with this action name
             [{ // and this array of custom arguments to create our entry
                 "language": language
